27182a35ab8bb4f55c6fc1dafbfd8425e197504e
Peter Goetsch <pgoetsch@dminc.com>
2018-02-25 12:11:20 +0200

    Write to logfile and read back

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,20 @@
 import socket
 from flask import Flask
+from datetime import datetime
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
+	hostname = socket.gethostname()
 	with open("/mnt/output.log", "a") as logfile:
-    	logfile.write("hello")
+    	logfile.write(str(datetime.now()) + " " + hostname)
 
-    return "Hello World! Greetings from " + socket.gethostname() + "\n"
+	logfile_content = ""
+	with open("/mnt/output.log", "r") as logfile:
+		logfile_content = logfile.read()
 
+    return "Hello World! Greetings from " + hostname + "\n\n\n" + logfile_content + "\n";
 
 if __name__ == "__main__":
     application.run()
