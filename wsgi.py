63de5a0c6a4f3945f54a722c9577370fc7f14f30
Peter Goetsch <pgoetsch@dminc.com>
2018-02-25 11:57:23 +0200

    Append to log file on each request

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,9 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    
+	with open("/mnt/output.log", "a") as logfile:
+    	logfile.write("hello")
+
     return "Hello World! Greetings from " + socket.gethostname() + "\n"
 
 
