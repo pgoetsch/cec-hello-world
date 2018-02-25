5438739129fdb71a4232c57cecac2f8cb25d4e57
Peter Goetsch <pgoetsch@dminc.com>
2018-02-25 11:33:35 +0200

    Testing webhook...

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     
-    return "Hello World! Greetings from "+socket.gethostname()+"\n"
+    return "Hello World! Greetings from " + socket.gethostname() + "\n"
 
 
 if __name__ == "__main__":
