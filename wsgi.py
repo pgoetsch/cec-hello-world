2ae8241ed51000cc81d672798b38c172833294e0
Peter Goetsch <pgoetsch@dminc.com>
2018-02-26 16:15:13 +0200

    Write newline to log

--- wsgi.py
+++ wsgi.py
@@ -8,14 +8,12 @@ application = Flask(__name__)
 def hello():
 	hostname = socket.gethostname()
 	with open("/mnt/output.log", "a") as logfile:
-		logfile.write(str(datetime.now()) + " " + hostname)
+		logfile.write(str(datetime.now()) + " " + hostname + "\n")
 
 	logfile_content = ""
 	with open("/mnt/output.log", "r") as logfile:
 		logfile_content = logfile.read()
 		return logfile_content;
-
-    #return "Hello World! Greetings from " + hostname + "\n\n\n" + logfile_content + "\n";
 
 if __name__ == "__main__":
     application.run()
